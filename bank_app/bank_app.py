6c5912ee68a8bbee7189abbfbfa94043a9323bc1
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-05 19:16:35 -0400

    Add BankAccount class + deposit method

---
+++ bank_app/bank_app.py
@@ -0,0 +1,12 @@
+class BankAccount(object):
+    def __init__(self, object):
+        self.name = object
+        self.balance = 0
+
+    def make_deposit(self, amount):
+        self.balance += amount
+
+
+bank_name=BankAccount('christian')
+
+print(bank_name.__dict__)
