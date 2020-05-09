52ae720d7a1d0d98b9b0080ce0352935ed36e446
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-09 09:28:22 -0400

    Create class + base methods

--- bank_app/bank_app.py
+++ bank_app/bank_app.py
@@ -9,7 +9,34 @@ class BankAccount(object):
     def make_withdrawal(self, amount):
         self.balance -= amount
 
+    def check_balance(self, amount):
+        if (self.balance - amount) >= 0:
+            print('insufficient funds')
 
-bank_name=BankAccount('christian')
+    def is_balance_positive(self):
+        if self.balance < 0:
+            print('you do not have a positive balance')
 
-print(bank_name.__dict__)
+
+# class BankAccount(object):
+#     def __init__(self, object):
+#         self.name = object
+#         self.balance = 0
+
+#     def make_deposit(self, amount):
+#         self.balance += amount
+
+#     def make_withdrawl(self, amount):
+#         self.balance -= amount
+
+#     def check_balance(self, amount):
+#         return (self.balance - amount) >= 0
+
+#     def is_positive_balance(self):
+#         return self.balance > 0
+
+christian=BankAccount('christian')
+
+christian.make_deposit(500)
+
+print(christian.__dict__)
