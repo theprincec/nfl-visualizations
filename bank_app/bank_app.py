52a31d58ef26aef79548619495d6569666240bbe
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-09 09:29:12 -0400

    same

--- bank_app/bank_app.py
+++ bank_app/bank_app.py
@@ -18,23 +18,6 @@ class BankAccount(object):
             print('you do not have a positive balance')
 
 
-# class BankAccount(object):
-#     def __init__(self, object):
-#         self.name = object
-#         self.balance = 0
-
-#     def make_deposit(self, amount):
-#         self.balance += amount
-
-#     def make_withdrawl(self, amount):
-#         self.balance -= amount
-
-#     def check_balance(self, amount):
-#         return (self.balance - amount) >= 0
-
-#     def is_positive_balance(self):
-#         return self.balance > 0
-
 christian=BankAccount('christian')
 
 christian.make_deposit(500)
